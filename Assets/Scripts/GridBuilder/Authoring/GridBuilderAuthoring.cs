46e8b60863f45b031dc1a067491582a6b64f7c05
DustoTail <dustotail@gmail.com>
2023-10-06 19:05:23 +0900

    Added a plane collider authoring

--- Assets/Scripts/GridBuilder/Authoring/GridBuilderAuthoring.cs
+++ Assets/Scripts/GridBuilder/Authoring/GridBuilderAuthoring.cs
@@ -2,22 +2,49 @@ using Unity.Entities;
 using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
+using Unity.Physics.Authoring;
+
 
 namespace StrengthInNumber.GridBuilder
 {
     public class GridBuilderAuthoring : MonoBehaviour
     {
         [Header("Grid")]
-        public int xCount;
-        public int yCount;
+        public int gridWidth;
+        public int gridHeight;
+        public Vector2 center;
+        public PhysicsShapeAuthoring groundCollider;
 
         [Header("Cell")]
         public float cellWidth;
         public float cellHeight;
 
+
         [Header("Debug")]
         // TODO: Add flag for adding differnt tags for debugging different parts of the framework
         public bool debug;
+        public bool drawGizmos;
+
+        private void OnValidate()
+        {
+            if(groundCollider != null)
+            {
+                groundCollider.SetPlane(
+                    new float3(center.x, 0f, center.y),
+                    new float2(gridWidth * cellWidth, gridHeight * cellHeight),
+                    Quaternion.identity);
+            }
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if(!drawGizmos)
+            {
+                return;
+            }
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireCube(center, new Vector3(gridWidth * cellWidth, 0.1f, gridHeight * cellHeight));
+        }
 
         public class GridBuilderBaker : Baker<GridBuilderAuthoring>
         {
@@ -33,35 +60,36 @@ namespace StrengthInNumber.GridBuilder
 
                 float cellWidth = authoring.cellWidth;
                 float cellHeight = authoring.cellHeight;
-                int xCount = authoring.xCount;
-                int yCount = authoring.yCount;
-                int length = xCount * yCount;
+                int gridWidth = authoring.gridWidth;
+                int gridHeight = authoring.gridHeight;
+                int arrayLength = gridWidth * gridHeight;
+
                 AddSharedComponent(self, new GridBuilder_GridBufferSettings()
                 {
                     cellWidth = cellWidth,
                     cellHeight = cellHeight,
-                    gridWidth = xCount,
-                    gridHeight = yCount
+                    gridWidth = gridWidth,
+                    gridHeight = gridHeight
                 });
 
-                // Grid bottom left (0,0) is world origin
-                var cells = new NativeArray<GridBuilder_GridBufferElement>(length, Allocator.Temp);
-                float2 offset = new float2(cellWidth, cellHeight) / 2f;
-                for (int y = 0; y < yCount; y++)
+                var cells = new NativeArray<GridBuilder_GridBufferElement>(arrayLength, Allocator.Temp);
+                float2 center = authoring.center;
+                float2 offset = new float2(cellWidth, cellHeight) / 2f - new float2(gridWidth * cellWidth, gridHeight * cellHeight) / 2f;
+                for (int y = 0; y < gridHeight; y++)
                 {
-                    for (int x = 0; x < xCount; x++)
+                    for (int x = 0; x < gridWidth; x++)
                     {
                         float xPosition = x * cellWidth;
                         float yPosition = y * cellHeight;
-                        int index = y * xCount + x;
+                        int index = y * gridWidth + x;
                         cells[index] = new GridBuilder_GridBufferElement
                         {
-                            position = new float2(xPosition, yPosition) + offset
+                            position = new float2(xPosition, yPosition) + center + offset
                         };
                     }
                 }
                var buffer = AddBuffer<GridBuilder_GridBufferElement>(self);
-                buffer.EnsureCapacity(length);
+                buffer.EnsureCapacity(arrayLength);
                 buffer.AddRange(cells);
                 cells.Dispose();
             }
